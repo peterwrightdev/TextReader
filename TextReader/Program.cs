f27aac6d232a5d036bbcf2c2df36a3cb31e907c5
peterwrightdev <peterwrightdev@users.noreply.github.com>
2020-07-31 10:33:45 +0100

    Initial commit

---
+++ TextReader/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace TextReader
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please supply the fully quantified path and name of the file to read");
+
+            string filename = Console.ReadLine();
+
+            Console.WriteLine("How many of the most frequent words do you want to know about? (default is 10)");
+
+            if (!int.TryParse(Console.ReadLine(), out int returnCount))
+            {
+                returnCount = 10;
+            }
+
+            try
+            {
+                List<KeyValuePair<string, int>> orderedWordCount = Program.GetOrderedWordCount(filename);
+                
+                Console.WriteLine(string.Format("{0} most frequent words are;", returnCount));
+
+                for (int i = 0; i < returnCount; i++)
+                {
+                    Console.WriteLine(orderedWordCount[i].Key + ": " + orderedWordCount[i].Value.ToString());
+                }
+            }
+            catch
+            {
+                Console.WriteLine(string.Format("Failed to read file {0}. Please use either include the full path of the file or ensure the file is in {1};", filename, Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase)));
+            }
+        }
+
+        public static List<KeyValuePair<string, int>> GetOrderedWordCount(string filename)
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string[] words = reader.ReadToEnd().Split(' ');
+                reader.Close();
+
+                // leverage Dictionary for handling of key uniqueness and lookup.
+                SafeDictionary<string> wordCount = new SafeDictionary<string>();
+
+                foreach (string word in words)
+                {
+                    wordCount.SafeIncrement(word);
+                }
+
+                // Order dictionary and convert to KeyValuePair as Dictionaries are not explicitly ordered.
+                return wordCount.OrderByDescending(x => x.Value).ToList();
+            }
+        }
+    }
+
+    // Define extension class on Dictionary to handle method to safely increment our count. If nothing for the key exists, initialise it.
+    // if a key value pair exists for the key, incrememnt the value by 1.
+    public class SafeDictionary<TKey> : Dictionary<TKey, int>
+    {
+        public void SafeIncrement(TKey keyToIncrement)
+        {
+            if (this.ContainsKey(keyToIncrement))
+            {
+                this[keyToIncrement]++;
+            }
+            else
+            {
+                this.Add(keyToIncrement, 1);
+            }
+        }
+    }
+}
---
+++ TextReaderTester/FileReaderTester.cs
@@ -0,0 +1,79 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using TextReader;
+
+namespace TextReaderTester
+{
+    [TestClass]
+    public class FileReaderTester
+    {
+        [TestMethod]
+        public void WhenProgramReadsFile_ThenCorrectWordCountReturned()
+        {
+            // Arrange
+            // Act
+            List<KeyValuePair<string, int>> orderdWordCount = TextReader.Program.GetOrderedWordCount("LotRAP.txt");
+
+            // Assert
+            // Verify in a vacuum that all values are ordered based on word count descending.
+            int count = 1;
+            foreach (KeyValuePair<string, int> orderedKeyValue in orderdWordCount)
+            {
+                if (count < orderdWordCount.Count)
+                {
+                    Assert.IsTrue(orderedKeyValue.Value >= orderdWordCount[count].Value);
+                    count++;
+                }
+            }
+
+            // As we have this specific file, we can verify this example explicitly.
+            Assert.AreEqual("the", orderdWordCount[0].Key);
+            Assert.AreEqual("of", orderdWordCount[1].Key);
+            Assert.AreEqual("and", orderdWordCount[2].Key);
+            Assert.AreEqual("to", orderdWordCount[3].Key);
+            Assert.AreEqual("in", orderdWordCount[4].Key);
+            Assert.AreEqual("a", orderdWordCount[5].Key);
+            Assert.AreEqual("that", orderdWordCount[6].Key);
+            Assert.AreEqual("is", orderdWordCount[7].Key);
+            Assert.AreEqual("his", orderdWordCount[8].Key);
+            Assert.AreEqual("will", orderdWordCount[9].Key);
+            Assert.AreEqual(3758, orderdWordCount[0].Value);
+            Assert.AreEqual(2168, orderdWordCount[1].Value);
+            Assert.AreEqual(1281, orderdWordCount[2].Value);
+            Assert.AreEqual(1093, orderdWordCount[3].Value);
+            Assert.AreEqual(750, orderdWordCount[4].Value);
+            Assert.AreEqual(738, orderdWordCount[5].Value);
+            Assert.AreEqual(487, orderdWordCount[6].Value);
+            Assert.AreEqual(406, orderdWordCount[7].Value);
+            Assert.AreEqual(397, orderdWordCount[8].Value);
+            Assert.AreEqual(394, orderdWordCount[9].Value);
+        }
+
+        [TestMethod]
+        public void SafeDictionaryCorrectlyIncrementsValuesForGivenKey()
+        {
+            SafeDictionary<string> testDictionary = new SafeDictionary<string>();
+
+            Assert.IsTrue(testDictionary.Count == 0);
+
+            testDictionary.SafeIncrement("string");
+
+            Assert.IsTrue(testDictionary.Count == 1);
+            Assert.AreEqual(1, testDictionary["string"]);
+
+            testDictionary.SafeIncrement("string");
+
+            Assert.IsTrue(testDictionary.Count == 1);
+            Assert.AreEqual(2, testDictionary["string"]);
+
+            testDictionary.SafeIncrement("string2");
+
+            Assert.IsTrue(testDictionary.Count == 2);
+            Assert.AreEqual(2, testDictionary["string"]);
+            Assert.AreEqual(1, testDictionary["string2"]);
+        }
+    }
+}
